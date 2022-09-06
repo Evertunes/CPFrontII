82cf8d189c4bb28d4124f4a6917904cd5cedf02d
Everton Alves <99445792+Evertunes@users.noreply.github.com>
2022-09-06 11:45:22 -0300

    Update function

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
-const email = document.querySelector("input.inputEmail");
+const email = document.querySelector("input.inputMail");
+const emailvalor = email.value;
 const senha = document.querySelector("input.inputPassword");
 const principal = document.querySelector("body");
 const formulario = document.getElementById("forms");
 
 function removaTodoEspaco() {
 
-document.querySelector('.inputMail').innerHTML = email;
-
-let str = email.replace(/\s+/g, '');
+document.querySelector('.email').innerHTML = emailvalor;
+let str = emailvalor.replace(/\s+/g, '');
 console.log(str);
-document.querySelector('.inputMail').textContent = str; 
+document.querySelector('emailvalor').textContent = str; 
 //document.querySelector('.inputMail').innerHTML = str;
 };
 
