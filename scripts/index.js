7e527937d4c1f4bd458c056cb3af032b8f96f94c
Everton Alves <alveseverton023@gmail.com>
2022-09-05 22:45:25 -0300

    funções

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+ 
+function RemoveTodoEspaco(){
+
+var email = ;
+
+document.querySelector('.default').innerHTML = strDefault;
+
+var str = strDefault.replace(/\s/g, '');
+
+document.querySelector('.new').innerHTML = str;
+};
+
+function RemoveEspaco(){
+
+    
+
+};
+
