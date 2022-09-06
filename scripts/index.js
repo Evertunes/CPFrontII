1582e9842bb3dd9fbe49deed632da5ae32c1a4df
Everton Alves <alveseverton023@gmail.com>
2022-09-06 20:11:48 -0300

    pegando dados

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,24 @@
-const email = document.querySelector("input.inputMail");
-const emailvalor = email.value;
+const email = document.getElementById("inputEmail");
 const senha = document.querySelector("input.inputPassword");
-const principal = document.querySelector("body");
 const formulario = document.getElementById("forms");
 
 function removaTodoEspaco() {
-
-document.querySelector('.email').innerHTML = emailvalor;
-let str = emailvalor.replace(/\s+/g, '');
-console.log(str);
-document.querySelector('emailvalor').textContent = str; 
+let digitado = document.getElementById("inputEmail").value;
+console.log(digitado);
+//document.querySelector('emailvalor').textContent = str; 
 //document.querySelector('.inputMail').innerHTML = str;
 };
 
 function RemoveEspaco(){
-
-    
-
 };
 
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    removaTodoEspaco();
+    let text1 = digitado;
+    let text2 = text1.split(" ").join("");
+    console.log(text2); 
+    //document.querySelector('.outputString').textContent = text2; 
+
+
+});
