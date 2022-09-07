a9f1842494cb236f9684378914cb155c46b885ee
Everton Alves <alveseverton023@gmail.com>
2022-09-06 21:27:40 -0300

    remover

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const formulario = document.getElementById("forms");
 function removaTodoEspaco() {
 let digitado = document.getElementById("inputEmail").value;
 console.log(digitado);
+ 
 //document.querySelector('emailvalor').textContent = str; 
 //document.querySelector('.inputMail').innerHTML = str;
 };
@@ -17,7 +18,7 @@ formulario.addEventListener("submit", (evento) => {
     removaTodoEspaco();
     let text1 = digitado;
     let text2 = text1.split(" ").join("");
-    console.log(text2); 
+    console.log(text2);
     //document.querySelector('.outputString').textContent = text2; 
 
 
