70be11e250ae33b2557f0cd24879742d38c8d4a4
Everton Alves <alveseverton023@gmail.com>
2022-09-07 08:30:14 -0300

    f

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,10 @@ const senha = document.querySelector("input.inputPassword");
 const formulario = document.getElementById("forms");
 
 function removaTodoEspaco() {
-let digitado = document.getElementById("inputEmail").value;
-console.log(digitado);
- 
-//document.querySelector('emailvalor').textContent = str; 
-//document.querySelector('.inputMail').innerHTML = str;
+document.querySelector('.inputMail').innerText;
 };
 
 function RemoveEspaco(){
 };
+
+removaTodoEspaco();
