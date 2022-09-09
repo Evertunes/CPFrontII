5f3a01fe5977024d3a1491f19f5289f4bf2309c1
Everton Alves <99445792+Evertunes@users.noreply.github.com>
2022-09-09 12:00:37 -0300

    Libera botao

--- scripts/index.js
+++ scripts/index.js
@@ -15,6 +15,20 @@ formulario.addEventListener('focusout', (evento) => {
   event.target.style.background = '';
 });
 
+//desabilita o botão no início 
+document.getElementById("botao").disabled = true;
+ //cria um event listener que escuta mudanças no input 
+document.getElementById("input").addEventListener("input", function(event){ 
+//busca conteúdo do input 
+var conteudo = document.getElementById("input").value; 
+//valida conteudo do input 
+if (conteudo !== null && conteudo !== '') { 
+//habilita o botão 
+document.getElementById("botao").disabled = false; 
+} 
+else {
+//desabilita o botão se o conteúdo do input ficar em branco 
+document.getElementById("botao").disabled = true; } });
 
 formulario.onsubmit = (event) => {
   event.preventDefault();
