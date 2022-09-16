38268a0bc19d49aa809c0d48629425e14eeb6d6d
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2022-09-15 22:12:07 -0300

    começando a trabalhar com a API

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,17 @@ let senha = document.getElementById("inputPassword");
 let errormessage = document.getElementById("erro");
 let textoerro = document.createTextNode("Não pode haver campos vazios!");
 let botao = document.querySelector(".sub");
+let urlTodo = "https://ctd-todo-api.herokuapp.com/v1";
 
 botao.disabled = true;
 
 function desabilita() {
   if (email || senha == "") {
     botao.disabled = true;
-  }
-  else {
+  } else {
     botao.disabled = false;
   }
-};
+}
 
 // Ao retirar o foco do input o mesmo fica vermelho indicando erro quando vazio
 formulario.addEventListener("focusout", (evento) => {
@@ -22,20 +22,19 @@ formulario.addEventListener("focusout", (evento) => {
   if (email.value == "" || senha.value == "") {
     evento.target.style.background = "red";
     desabilita();
-  }
-  else {
+  } else {
   }
 });
 
 email.addEventListener("focusout", () => {
-  if(email.value == ""){
+  if (email.value == "") {
     errormessage.appendChild(textoerro);
     errormessage.style.color = "red";
   }
 });
 
 senha.addEventListener("focusout", () => {
-  if(senha.value == ""){
+  if (senha.value == "") {
     errormessage.appendChild(textoerro);
     errormessage.style.color = "red";
   }
@@ -43,20 +42,38 @@ senha.addEventListener("focusout", () => {
 
 formulario.addEventListener("keypress", (evento) => {
   //evento.preventDefault();
-    evento.target.style.background = "";
-    botao.disabled = false;
-  }
-);
+  evento.target.style.background = "";
+  botao.disabled = false;
+});
 
-formulario.onsubmit = (evento) => {
-    formulario.removeChild(errormessage);
+formulario.onsubmit = () => {
+  formulario.removeChild(errormessage);
   // Por ser do tipo email, o campo não permite espaços entre a digitação apenas no começo e fim, por isso usamos o metodo "trim";
   let normalizaEmail = email.value.trim();
   //O campo senha por sua vez, permite espaços aleatórios entre os caracteres, por esse motivo usamos o "replace";
   let normalizaSenha = senha.value.replace(/ /g, "");
   console.log(`${normalizaEmail}`, `${normalizaSenha}`);
-    setTimeout(() => {
-      email.value = null;
-      senha.value = null;
-    }, 0.5 * 1000);
+  setTimeout(() => {
+    email.value = null;
+    senha.value = null;
+  }, 0.5 * 1000);
 };
+
+// login do usuário
+const login = fetch(`${urlTodo}/users/login`, {
+  method: "POST",
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({ email: "everton@gmail.com", password: "enha" }),
+})
+  .then((response) => response.json())
+  .then((dados) => {
+    console.log(dados);
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+
+  
