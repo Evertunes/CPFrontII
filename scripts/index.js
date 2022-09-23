0110e62ff4a0a412fab00f74bf9eb40d46955b7a
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2022-09-22 23:04:15 -0300

    ajustes nos campos email e senha

--- scripts/index.js
+++ scripts/index.js
@@ -2,108 +2,85 @@ let formulario = document.getElementById("formulario");
 let email = document.getElementById("inputEmail");
 let senha = document.getElementById("inputPassword");
 let errormessage = document.getElementById("erro");
-let textoerro = document.createTextNode("Não pode haver campos vazios!");
+let textoerro = document.createTextNode("Existe(m) campo(s) inválido(s)!");
 let errofetch = document.createTextNode("Erro de login!");
 let botao = document.querySelector(".sub");
 let urlTodo = "https://ctd-todo-api.herokuapp.com/v1";
 const header = { "Content-type": "application/json; charset=UTF-8" };
-botao.disabled = true
-function desabilita() {
-  if (email || senha == "") {
-    botao.disabled = true;
-  } else {
-    botao.disabled = false;
-  }
-}
+let emailOk = false;
+let senhaOk = false;
+botao.disabled = true;
 
 function validateEmail(email) {
   var re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
 
+function validaBotao() {
+  if (emailOk && senhaOk) {
+    botao.disabled = false;
+  } else {
+    botao.disabled = true;
+  }
+}
+
 email.onkeyup = (evento) => {
-  if (email.value == "") {
+  email.value = email.value.replace(/ /g, "");
+  if (validateEmail(email.value)) {
+    errormessage.innerText = "";
+    evento.target.style.background = "";
+    emailOk = true;
+  } else {
+    evento.target.style.background = "pink";
     errormessage.appendChild(textoerro);
     errormessage.style.color = "red";
-  } else {
-    evento.target.style.background = "";
+    emailOk = false;
   }
+  validaBotao();
 };
 
 senha.onkeyup = (evento) => {
-  if (senha.value == "") {
-    errormessage.appendChild(textoerro);
-    errormessage.style.color = "red";
-  } else {
+  senha.value = senha.value.replace(/ /g, "");
+  if (senha.value !== "") {
+    errormessage.innerText = "";
     evento.target.style.background = "";
-  }
-};
-
-email.onblur = (evento) => {
-  if (email.value !== "" && validateEmail(email.value)) {
-    if (errormessage == true) {
-      errormessage.removeChild(textoerro);
-    }
+    senhaOk = true;
   } else {
     evento.target.style.background = "pink";
     errormessage.appendChild(textoerro);
     errormessage.style.color = "red";
+    senhaOk = false;
   }
-};
-
-senha.onkeyup = () => {
-  if (senha.value !== "" ) {
-    botao.disabled = false;
-  } 
-}
-
-senha.onblur = (evento) => {
-  if (senha.value !== "" && errormessage == true) {
-    errormessage.removeChild(textoerro);
-  } 
-  else {
-    evento.target.style.background = "pink";
-    errormessage.appendChild(textoerro);
-    errormessage.style.color = "red";
-  }
+  validaBotao();
 };
 
 formulario.onsubmit = () => {
   formulario.removeChild(errormessage);
-  let normalizaEmail = email.value.trim();
-  let normalizaSenha = senha.value.replace(/ /g, "");
-  console.log(`${normalizaEmail}`, `${normalizaSenha}`);
 
   // login do usuário
   const bodyLogin = JSON.stringify({
-    email: emailR.value,
-    password: senhaR.value
-  })
-.then(async response => {
-
-    if(response.status === 201){
+    email: email.value,
+    password: senhaR.value,
+  }).then(async (response) => {
+    if (response.status === 201) {
       let body = await response.json();
       let token = body.jwt;
 
-      sessionStorage.setItem("token", token)
+      sessionStorage.setItem("token", token);
     }
 
-    if(response.status === 400){
+    if (response.status === 400) {
       function myFunction() {
         var x = document.getElementById("erro");
-          x.innerHTML = "Login Failed, try again!";
-          textoERR.style.color = "red";
-        }
-      myFunction();
+        x.innerHTML = "Login Failed, try again!";
+        textoERR.style.color = "red";
       }
-  
-  
-    })
+      myFunction();
     }
+  });
+};
 
-  setTimeout(() => {
-    email.value = null;
-    senha.value = null;
-  }, 0.5 * 1000);
-
-
+setTimeout(() => {
+  email.value = null;
+  senha.value = null;
+}, 0.5 * 1000);
